66fa7b891c867a417ba6baf9350fee3a3dd1527b
kasimsha <kasimsha.2842@gmail.com>
2021-08-16 13:31:18 +0530

    version2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './components/user.component';
 import { UserListComponent } from './components/userList.component';
+//import { ModelComponent } from './model.component';
 
 const routes: Routes = [
   { path: 'signup', component: UserComponent },
   { path: 'userList', component: UserListComponent },
-  // { path: 'hospitalList', component: HospitalListComponent },
+  //{ path: 'model', component: ModelComponent },
   // { path: 'hospital', component: HospitalComponent },
   // { path: 'login', component: LoginComponent },
 ];
