e7ff3f0319a6ea45563590478bb5a63e1a7ad3b9
kasimsha <kasimsha.2842@gmail.com>
2021-08-16 22:52:14 +0530

    latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home.component';
 import { HospitalComponent } from './components/hospital.component';
 import { HospitalListComponent } from './components/hospitalList.component';
+import { LoginComponent } from './components/login.component';
 import { UserComponent } from './components/user.component';
 import { UserListComponent } from './components/userList.component';
 
@@ -12,6 +13,8 @@ const routes: Routes = [
   { path: 'hospital', component: HospitalComponent },
   { path: 'hospitalList', component: HospitalListComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
 ];
 
 @NgModule({
